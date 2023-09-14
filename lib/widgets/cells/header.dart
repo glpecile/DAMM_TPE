16707dac1b9c3d21e6c098bb2a4f4de68626867c
Ana Tarantino <atarantino@itba.edu.ar>
2023-09-14 17:01:10 -0300

    fix: tab colors

--- lib/widgets/cells/header.dart
+++ lib/widgets/cells/header.dart
@@ -1,31 +1,49 @@
 import 'package:SerManos/widgets/tokens/colors.dart';
 import 'package:flutter/material.dart';
 
-import '../../main.dart';
-
 class Header extends StatelessWidget implements PreferredSizeWidget{
   const Header({super.key});
+
+  TabBar get _tabBar => const TabBar(
+    indicator: BoxDecoration(
+      color: SerManosColors.secondary_200,
+      border: Border(
+        bottom: BorderSide(
+          color: SerManosColors.neutral_0,
+          width: 2.0,
+        ),
+      ),
+    ),
+    tabs: [
+      Tab(child: Text('Postularse')),
+      Tab(child: Text('Mi perfil')),
+      Tab(child: Text('Novedades')),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       centerTitle: false,
       title: const Image(
-              image: AssetImage("assets/images/logo_with_text.png"),
-              height: 25,
-              fit: BoxFit.contain,
-            ),
+        image: AssetImage("assets/images/logo_with_text.png"),
+        height: 25,
+        fit: BoxFit.contain,
+      ),
       backgroundColor: SerManosColors.secondary_90,
-      bottom: const TabBar(
-              tabs: [
-              Tab(child: Text('Postularse')),
-              Tab(child: Text('Mi perfil')),
-              Tab(child: Text('Novedades')),
-              ],
-            ),
+      bottom: PreferredSize(
+        preferredSize: _tabBar.preferredSize,
+        child: Material(
+            color: SerManosColors.secondary_100,
+            child:  Theme( // <-- SEE HERE
+              data: ThemeData(
+              ).copyWith(appBarTheme:const AppBarTheme()),
+              child: _tabBar),
+        ),
+      ),
     );
   }
 
   @override
-  Size get preferredSize => new Size.fromHeight(kToolbarHeight+kTextTabBarHeight);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight+kTextTabBarHeight);
 }
