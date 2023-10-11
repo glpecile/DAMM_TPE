88d178a5cbcabb6bcdc983b8e26a4dfc211978c5
Gian <gpecile@itba.edu.ar>
2023-10-10 23:21:06 -0300

    refactor: remove hardcoded values on card news

--- lib/widgets/cells/cards/card_news.dart
+++ lib/widgets/cells/cards/card_news.dart
@@ -5,73 +5,80 @@ import '../../tokens/colors.dart';
 import '../../tokens/typography.dart';
 
 class CardNews extends StatelessWidget {
-  const CardNews({super.key});
+  // TODO: swap for model with news data
+  final String overline;
+  final String title;
+  final String description;
+  final String? imageUrl;
+
+  const CardNews({
+    Key? key,
+    required this.overline,
+    required this.title,
+    required this.description,
+    this.imageUrl,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return InkWell(
       child: Container(
-        width: 328,
-        height: 156,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(2),
-            color: SerManosColors.neutral_0,
-            boxShadow: SerManosShadows.shadow_2),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(6),
-          child: Row(children: [
-            Column(children: [
+          borderRadius: BorderRadius.circular(2),
+          color: SerManosColors.neutral_0,
+          boxShadow: SerManosShadows.shadow_2,
+        ),
+        child: IntrinsicHeight(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
               Image.asset(
                 'assets/images/donante.jpg',
                 width: 118,
-                height: 156,
+                fit: BoxFit.cover,
               ),
-            ]),
-            Expanded(
-              child: Column(
-                children: [
-                  const Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(8, 16, 8, 0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("REPORTE 2820",
-                              style: SerManosTypography.overline()),
-                          Text(
-                            "Ser donante voluntario",
-                            style: SerManosTypography.subtitle_01(
-                                color: SerManosColors.neutral_100),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(
-                                left: 0, bottom: 8, right: 0, top: 0),
-                            child: Text(
-                                "Desde el Hospital Centenario recalcan la importancia de la donación voluntaria de Sangre",
-                                style: SerManosTypography.body_02()),
-                          ),
-                        ],
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 16, 8, 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        overline,
+                        style: const SerManosTypography.overline(),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0,0,8,0),
-                    child: Column(
-                      children: [
-                        Align(
-                            alignment: Alignment.bottomRight,
-                            child: (
-                                ButtonCTA(btnColor: SerManosColors.primary_100, text: "Leer Más", onPressed: () {  },foregroundColor: SerManosColors.neutral_75,)
-                            )
+                      Text(
+                        title,
+                        style: const SerManosTypography.subtitle_01(
+                          color: SerManosColors.neutral_100,
                         ),
-                      ],
-                    ),
-                  )
-                ],
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                      Text(
+                        description,
+                        style: const SerManosTypography.body_02(),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 3,
+                      ),
+                      const SizedBox(height: 8),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: ButtonCTA(
+                          btnColor: SerManosColors.primary_100,
+                          text: "Leer Más",
+                          onPressed: () {},
+                          foregroundColor: SerManosColors.neutral_75,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ]),
+            ],
+          ),
         ),
       ),
     );
