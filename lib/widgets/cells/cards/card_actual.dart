0fd0cb1611d72ef1095c4243d17a64b2b81676e7
Ana Tarantino <atarantino@itba.edu.ar>
2023-11-17 00:22:06 -0300

    Auto stash before merge of "main" and "origin/main"
    Co-authored-by: Gian <gpecile@itba.edu.ar>
    Co-authored-by: Mati <mlombardi@itba.edu.ar>

--- lib/widgets/cells/cards/card_actual.dart
+++ lib/widgets/cells/cards/card_actual.dart
@@ -14,36 +14,51 @@ class CardVolunteersActual extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-            width: 328,
-            height: 72,
-            decoration: BoxDecoration(
-                border: Border.all(
-                    color: SerManosColors.primary_100,
-                    width: 2
-                ),
-                borderRadius: BorderRadius.circular(6),
-                color: SerManosColors.primary_5,
-                boxShadow: SerManosShadows.shadow_2),
-            child: ClipRRect(
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
+    return Column(
+        children: [
+          const Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: EdgeInsets.only(
+                  left: 16, bottom: 24, top: 24, right: 16),
+              child: Text(
+                'Tu actividad',
+                style: SerManosTypography.headline_01(),
+              ),
+            ),
+          ),
+          Center(
+              child: Container(
+                  width: 328,
+                  height: 72,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: SerManosColors.primary_100,
+                          width: 2
+                      ),
+                      borderRadius: BorderRadius.circular(6),
+                      color: SerManosColors.primary_5,
+                      boxShadow: SerManosShadows.shadow_2),
+                  child: ClipRRect(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              const Text("ACCIÓN SOCIAL", style: SerManosTypography.overline()),
-                              Text(title, style: const SerManosTypography.subtitle_01(color: SerManosColors.neutral_100),
-                              ),
-                            ]),
-                        const Icon(SerManosIcons.location,color: SerManosColors.primary_100,)
-                      ]
-                  ),
-                ))));
+                              Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text("ACCIÓN SOCIAL", style: SerManosTypography.overline()),
+                                    Text(title, style: const SerManosTypography.subtitle_01(color: SerManosColors.neutral_100),
+                                    ),
+                                  ]),
+                              const Icon(SerManosIcons.location,color: SerManosColors.primary_100,)
+                            ]
+                        ),
+                      )))),
+        ]
+    );
   }
 
 }
