88e6fb06326e46f4745dd6df38c925b22f6d2e53
Matías Federico Lombardi <mflombardi2000@gmail.com>
2023-11-17 00:04:14 -0300

    fix: analytics. Co-authored-by: Gian <gpecile@itba.edu.ar> Co-authored-by: Anita <atarantino@itba.edu.ar>

--- lib/services/analytics_service.dart
+++ lib/services/analytics_service.dart
@@ -1,13 +1,17 @@
+import 'dart:developer';
+
 import 'package:firebase_analytics/firebase_analytics.dart';
 
 class AnalyticsService {
   Future<void> applyToVolunteeringEvent(String volunteeringId, String volunteerId) async {
+    log("ABOUT TO APPLY TO VOLUNTEERING");
     await FirebaseAnalytics.instance.logEvent(
         name: "applied_to_volunteering",
         parameters: {"volunteering": volunteeringId, "volunteer": volunteerId});
   }
 
   Future<void> leaveVolunteeringEvent(String volunteeringId, String volunteerId) async {
+    log("ABOUT TO LEAVE VOLUNTEERING");
     await FirebaseAnalytics.instance.logEvent(
         name: "leaft_volunteering",
         parameters: {"volunteering": volunteeringId, "volunteer": volunteerId});
